fb4cbbc821a6911c588b6eeadfc96ded64942b7f
Icaro <icarobn@gmail.com>
2013-06-16 16:25:39 -0300

    Tela de configuração
    Início do desenvolvimento da tela.

--- gen/br/com/upinterativo/upstopwatch/R.java
+++ gen/br/com/upinterativo/upstopwatch/R.java
@@ -14,10 +14,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f070004;
+        public static final int textView1=0x7f070000;
+        public static final int textView2=0x7f070002;
+        public static final int timePicker1=0x7f070001;
+        public static final int timePicker2=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main_stopwatch=0x7f030000;
+        public static final int conf=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main_stopwatch=0x7f060000;
@@ -25,6 +30,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040002;
+        public static final int label_fakeTime=0x7f040004;
+        public static final int label_realTime=0x7f040003;
         public static final int menu_settings=0x7f040001;
     }
     public static final class style {
