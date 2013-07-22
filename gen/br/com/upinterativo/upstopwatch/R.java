1bd3663cccd89a347d51991050ac70020fc92411
Icaro <icarobn@gmail.com>
2013-07-22 18:04:00 -0300

    Strings

--- gen/br/com/upinterativo/upstopwatch/R.java
+++ gen/br/com/upinterativo/upstopwatch/R.java
@@ -26,6 +26,13 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040002;
+        public static final int init_miliseconds=0x7f040007;
+        public static final int init_minute=0x7f040005;
+        public static final int init_second=0x7f040006;
+        public static final int label_fakeTime=0x7f040004;
+        public static final int label_realTime=0x7f040003;
+        public static final int lbl_minutes=0x7f040008;
+        public static final int lbl_seconds=0x7f040009;
         public static final int menu_settings=0x7f040001;
     }
     public static final class style {
