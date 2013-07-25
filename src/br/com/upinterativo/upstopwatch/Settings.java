cc324f5d6be797ed3603f5807e94879f15982c14
Icaro <icarobn@gmail.com>
2013-07-25 19:44:38 -0300

    Finalizado primeira parte da tela de configuracoes
    A tela esta com os dados de minutos para tempo real e falso.

--- src/br/com/upinterativo/upstopwatch/Settings.java
+++ src/br/com/upinterativo/upstopwatch/Settings.java
@@ -2,12 +2,133 @@ package br.com.upinterativo.upstopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Settings  extends Activity {
+	
+	private Button btPlusRealTime;
+	private Button btLessRealTime;
+	private Button btPlusFakeTime;
+	private Button btLessFakeTime;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        
+        /*Botoes de interacao com o tempo real*/
+        
+        btPlusRealTime = (Button)findViewById(R.id.btPlusRealTime);
+        btLessRealTime = (Button)findViewById(R.id.btLessRealTime);
+        
+        /*Botoes de interacao com o tempo falso*/
+        
+        btPlusFakeTime = (Button)findViewById(R.id.btPlusFakeTime);
+        btLessFakeTime = (Button)findViewById(R.id.btLessFakeTime);
+        
+        /*Definindo cor transparente aos botoes*/
+        
+        btPlusRealTime.setBackgroundColor(0);
+        btLessRealTime.setBackgroundColor(0);
+        btPlusFakeTime.setBackgroundColor(0);
+        btLessFakeTime.setBackgroundColor(0);
+        
+        /*Evento de clique para incrementar minutos do tempo real*/
+        
+        btPlusRealTime.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				EditText valueRealTime = (EditText)findViewById(R.id.vl_realTime);
+				int value = Integer.parseInt(valueRealTime.getText().toString());
+				value++;
+				String newValue = "";
+				
+				if (value <= 99) {
+					if (value < 10) {
+						newValue = "0" + value;
+					}
+					else {
+						newValue = "" + value;
+					}
+					
+					valueRealTime.setText(newValue);
+				}
+			}
+		});
+        
+        /*Evento de clique para decrementar minutos do tempo real*/
+        
+        btLessRealTime.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				EditText valueRealTime = (EditText)findViewById(R.id.vl_realTime);
+				int value = Integer.parseInt(valueRealTime.getText().toString());
+				value--;
+				String newValue = "";
+				
+				if (value >= 0) {
+					if (value < 10) {
+						newValue = "0" + value;
+					}
+					else {
+						newValue = "" + value;
+					}
+					
+					valueRealTime.setText(newValue);
+				}
+			}
+		});
+        
+        /*Evento de clique para incrementar minutos do tempo falso*/
+        
+        btPlusFakeTime.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				EditText valueRealTime = (EditText)findViewById(R.id.vl_fakeTime);
+				int value = Integer.parseInt(valueRealTime.getText().toString());
+				value++;
+				String newValue = "";
+				
+				if (value <= 99) {
+					if (value < 10) {
+						newValue = "0" + value;
+					}
+					else {
+						newValue = "" + value;
+					}
+					
+					valueRealTime.setText(newValue);
+				}
+			}
+		});
+        
+        /*Evento de clique para decrementar minutos do tempo real*/
+        
+        btLessFakeTime.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				EditText valueRealTime = (EditText)findViewById(R.id.vl_fakeTime);
+				int value = Integer.parseInt(valueRealTime.getText().toString());
+				value--;
+				String newValue = "";
+				
+				if (value >= 0) {
+					if (value < 10) {
+						newValue = "0" + value;
+					}
+					else {
+						newValue = "" + value;
+					}
+					
+					valueRealTime.setText(newValue);
+				}
+			}
+		});
     }
 }
