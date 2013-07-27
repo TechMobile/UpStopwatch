434a10270cb19483d279e9d36ad783bc2fe3b6df
Icaro <icarobn@gmail.com>
2013-07-27 16:20:31 -0300

    Mudança de lado dos botões para configurar os minutos

--- gen/br/com/upinterativo/upstopwatch/R.java
+++ gen/br/com/upinterativo/upstopwatch/R.java
@@ -23,10 +23,10 @@ public final class R {
         public static final int watchstop_background_green=0x7f020005;
     }
     public static final class id {
-        public static final int btLessFakeTime=0x7f080008;
-        public static final int btLessRealTime=0x7f080004;
-        public static final int btPlusFakeTime=0x7f080006;
-        public static final int btPlusRealTime=0x7f080002;
+        public static final int btLessFakeTime=0x7f080006;
+        public static final int btLessRealTime=0x7f080002;
+        public static final int btPlusFakeTime=0x7f080008;
+        public static final int btPlusRealTime=0x7f080004;
         public static final int chAlarm=0x7f080009;
         public static final int lb_fakeTime=0x7f080005;
         public static final int lb_realTime=0x7f080001;
